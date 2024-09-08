d92dd63f24d4b45fd3e2d50eccd391df105b9d45
Santiago Fernandez <dfernandezd@unicauca.edu.co>
2024-09-07 20:10:24 -0500

    Base proyecto Spring (Dependecias incluidas)

---
+++ src/main/java/com/activate/ActivateDDD/ActivateDddApplication.java
@@ -0,0 +1,13 @@
+package com.activate.ActivateDDD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivateDddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivateDddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/activate/ActivateDDD/ActivateDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activate.ActivateDDD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivateDddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
